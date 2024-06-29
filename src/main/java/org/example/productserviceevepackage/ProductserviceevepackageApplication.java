1bd1d5f30e9e8a41bcb7daf7fcd4d49d246ba380
anjukashyap <anjukashyap109@gmail.com>
2024-06-29 18:50:59 +0530

    commiting my product code

---
+++ src/main/java/org/example/productserviceevepackage/ProductserviceevepackageApplication.java
@@ -0,0 +1,13 @@
+package org.example.productserviceevepackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductserviceevepackageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductserviceevepackageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/productserviceevepackage/ProductserviceevepackageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.productserviceevepackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductserviceevepackageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
